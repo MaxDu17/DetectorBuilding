0504f0d073547e971951fb563b9a0dbfd518f334
MaximilianDu <32785271+MaxDu17@users.noreply.github.com>
2019-11-01 16:49:31 -0400

    bingk oweh

--- PythonSerial/SerialReader.py
+++ PythonSerial/SerialReader.py
@@ -1,1 +1,15 @@
-import pySerial
+
+import serial
+ser = serial.Serial(port='COM4', baudrate=9600)
+'''
+ser.parity = serial.PARITY_ODD
+ser.port.open()
+ser.port.close()
+ser.port.parity = serial.PARITY_NONE
+ser.port.close()
+'''
+#k = open("test.txt", "w")
+while(true):
+    s = str(ser.readline())
+    print(s)
+
