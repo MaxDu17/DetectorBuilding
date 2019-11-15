159548907314dc219f47d4c41c18cd23917f52e1
MaximilianDu <32785271+MaxDu17@users.noreply.github.com>
2019-11-15 16:13:11 -0500

    wheetus

--- PythonSerial/SerialReader.py
+++ PythonSerial/SerialReader.py
@@ -1,11 +1,19 @@
 
 import serial
+import csv
 ser = serial.Serial(port='COM4', baudrate=9600)
 
-#k = open("test.txt", "w")
-while(True):
+k = open("calibration.csv", "w")
+dataWriter = csv.writer(k, lineterminator = "\n")
+
+for i in range(10):
     s = str(ser.readline())
-    _, k, __ = s.split("'")
-    q = k.split("\\")
-    print(q[0])
+    s = s.replace("b'", '')
+    s = s.replace("\\r\\n'", '')
+    a, b = s.split("&")
+    dataWriter.writerow([a, b])
+    print(a + "\t" + b)
+
+
+
 
