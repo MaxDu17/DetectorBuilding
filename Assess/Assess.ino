6b1aee34e0184ccd4d73205e69df77220d7f963c
MaximilianDu <32785271+MaxDu17@users.noreply.github.com>
2019-11-22 16:21:28 -0500

    eee

---
+++ Assess/Assess.ino
@@ -0,0 +1,115 @@
+#include <OneWire.h> 
+#include <DallasTemperature.h>
+#define ONE_WIRE_BUS 2 
+#define REDPIN 13
+#define GREENPIN 12
+#define BLUEPIN 11
+
+OneWire oneWire(ONE_WIRE_BUS); 
+float calibrated[5];
+double avg; 
+DallasTemperature sensors(&oneWire);
+double raw; 
+
+String in;
+
+void setup(void) 
+{ 
+  Serial.begin(9600); 
+  pinMode(A0, INPUT);
+  pinMode(REDPIN, OUTPUT);
+  pinMode(GREENPIN, OUTPUT);
+  pinMode(BLUEPIN, OUTPUT); 
+  String status_  = ""; 
+  while(!status_.equals("go"))
+  {
+    Serial.println("ready");
+    status_ = Serial.readString(); 
+    status_.trim(); 
+  }
+  Serial.println("RECEIVED HANDSHAKE");
+} 
+
+void loop(void) 
+{ 
+  avg = 0; 
+ sensors.requestTemperatures(); // Send the command to get temperature readings 
+ for(int i = 0; i < 5; i ++)
+ {
+  calibrated[i] = sensors.getTempCByIndex(i);
+  avg += sensors.getTempCByIndex(i);
+  if(calibrated[i] == -127)
+  {
+    Serial.println("pullup");
+  }
+ }
+ avg = avg/5;
+ raw = analogRead(A0);
+ //Serial.print("This is the average"); 
+ String rawS = String(raw);  
+ Serial.println(rawS + "&" + avg); 
+
+ 
+ in = Serial.readString();
+ in.trim();
+ if(in.equals("R"))
+ {
+    digitalWrite(REDPIN, HIGH);
+    digitalWrite(GREENPIN, LOW);
+    digitalWrite(BLUEPIN, LOW);
+   
+ }
+ else if(in.equals("G"))
+ {
+  digitalWrite(REDPIN, LOW);
+    digitalWrite(GREENPIN, HIGH);
+    digitalWrite(BLUEPIN, LOW);
+    
+ }
+  else if(in.equals("B"))
+ {
+ digitalWrite(REDPIN, LOW);
+    digitalWrite(GREENPIN, LOW);
+    digitalWrite(BLUEPIN, HIGH);
+    
+ }
+
+  else if(in.equals("RG"))
+  {
+digitalWrite(REDPIN, HIGH);
+    digitalWrite(GREENPIN, HIGH);
+    digitalWrite(BLUEPIN, LOW);
+    
+  }
+    else if(in.equals("RB"))
+  {
+  digitalWrite(REDPIN, HIGH);
+    digitalWrite(GREENPIN, LOW);
+    digitalWrite(BLUEPIN, HIGH);
+   
+  }
+    else if(in.equals("BG"))
+  {
+ digitalWrite(REDPIN, LOW);
+    digitalWrite(GREENPIN, HIGH);
+    digitalWrite(BLUEPIN, HIGH);
+   
+  }
+      else if(in.equals("RGB"))
+  {
+  digitalWrite(REDPIN, HIGH);
+    digitalWrite(GREENPIN, HIGH);
+    digitalWrite(BLUEPIN, HIGH);
+   
+  }
+     else if(in.equals("OFF"))
+  {
+  digitalWrite(REDPIN, LOW);
+    digitalWrite(GREENPIN, LOW);
+    digitalWrite(BLUEPIN, LOW);
+   
+  }
+
+ 
+ 
+}
--- PythonSerial/Assess.py
+++ PythonSerial/Assess.py
@@ -3,50 +3,51 @@ import serial
 import csv
 from Model import Model
 import time
+
 myModel = Model()
-valuedict = { #note: add 7 to the original value to get the lower bound
-    1: -1, #R_low
-    2 : -1, #G_low
-    3 : -1, #B_low
-    4 : -1, #"RG_low
-    5 : -1,#"RB_low
-     6: -1,#"BG_low
-    7: -1,#"RGB_low
-     8: -1,#"R_up
-    9: -1,#"G_up
-     10: -1,#"B_up
-     11: -1,#"RG_up
-     12: -1,#"RB_up
-    13: -1,#"BG_up
-     14: -1#"RGB_up
+valuedict = {  # note: add 7 to the original value to get the lower bound
+    1: -1,  # R_low
+    2: -1,  # G_low
+    3: -1,  # B_low
+    4: -1,  # "RG_low
+    5: -1,  # "RB_low
+    6: -1,  # "BG_low
+    7: -1,  # "RGB_low
+    8: -1,  # "R_up
+    9: -1,  # "G_up
+    10: -1,  # "B_up
+    11: -1,  # "RG_up
+    12: -1,  # "RB_up
+    13: -1,  # "BG_up
+    14: -1  # "RGB_up
 }
 
 translationdict = {
-1: "R",
-    2 : "G",
-    3 : "B",
-    4 : "RG",
-    5 : "RB",
-     6: "BG",
+    1: "R",
+    2: "G",
+    3: "B",
+    4: "RG",
+    5: "RB",
+    6: "BG",
     7: "RGB"
 
 }
 print("-----------SETUP------------")
 status = input("load from previous? (y, n, s)")
-if(status == "y"):
+if (status == "y"):
     try:
         valuedict = pickle.load(open("RANGES.pkl", "rb"))
     except:
         print("whoops, you might not have made a file!")
         quit()
 
-elif(status == "n"):
+elif (status == "n"):
     for i in range(1, 8):
         valuedict[i] = float(input(translationdict[i] + "_low?"))
-        valuedict[i+7] = float(input(translationdict[i] + "_high?"))
+        valuedict[i + 7] = float(input(translationdict[i] + "_high?"))
     status = input("save for later? (y, n)")
-    if(status == "y"):
-        pickle.dump( valuedict, open( "RANGES.pkl", "wb" ) )
+    if (status == "y"):
+        pickle.dump(valuedict, open("RANGES.pkl", "wb"))
 
 try:
     ser = serial.Serial(port='COM4', baudrate=9600)
@@ -70,28 +71,29 @@ count = 0
 while True:
 
     s = myModel.parseSerial(ser.readline())
-    
-    value = int(s)
+    s, truth = s.split("&")
+    truth = float(truth)
+    value = float(s)
     voltage = myModel.toVoltage(value)
     temperature = myModel.voltageToTemp((voltage))
 
-    print("The voltage is: " + str(round(voltage,2)) + "V. The calculated temperature is: " + str(round(temperature,2)) + " degrees Celsius")
+    print("Calculated temp: " + str(
+        round(temperature, 2)) + "\t real temp: " + str(truth) + "\t the diff: " + str(truth - temperature))
 
-    if(change):
+    if (change):
         print("\tswitching color to: " + sendstatus)
         ser.write(sendstatus.encode('utf-8'))
         change = False
 
-
-    for i in range(1,8):
+    for i in range(1, 8):
         lowerbound = valuedict[i]
         higherbound = valuedict[i + 7]
-        if(lowerbound  == -1 or higherbound == -1):
+        if (lowerbound == -1 or higherbound == -1):
             continue
 
         if temperature > lowerbound and temperature < higherbound:
             carrier = translationdict[i]
-            if(carrier != laststatus):
+            if (carrier != laststatus):
                 sendstatus = carrier
                 laststatus = carrier
                 change = True
