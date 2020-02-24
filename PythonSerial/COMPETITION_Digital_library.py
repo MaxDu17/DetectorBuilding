2524d0d39f4d11acd669f54d6031ac0beea2e7b7
MaximilianDu <32785271+MaxDu17@users.noreply.github.com>
2020-02-24 17:14:49 -0500

    Update COMPETITION_Digital_library.py

--- PythonSerial/COMPETITION_Digital_library.py
+++ PythonSerial/COMPETITION_Digital_library.py
@@ -3,8 +3,9 @@ import serial
 from Model import Model
 import time
 myModel = Model()
-from serial.tools import list_ports
 
+from SerialLibrary import SerialLibrary
+serialHelp = SerialLibrary()
 BIAS = 0
 
 valuedict = { #note: add 7 to the original value to get the lower bound
@@ -39,7 +40,6 @@ def setup():
     global ser
     print("-----------SETUP------------")
 
-
     status = input("load from previous? (y, n, s)")
     if(status == "y"):
         try:
@@ -56,46 +56,8 @@ def setup():
         if(status == "y"):
             pickle.dump( valuedict, open( "RANGES.pkl", "wb" ) )
 
-    try:
-        ser = serial.Serial(port='COM5', baudrate=9600)
-        #ser = serial.Serial(port='COM8', baudrate=9600)
-    except:
-        print("sorry, this port is busy or not correct. double check programs!")
-        ports = list(list_ports.comports())
-        print("here are the available ports: " + str([k.device for k in ports]))
-        quit()
-
-    semantic = "no"
-    while semantic != "ready":
-        semantic = myModel.parseSerial(ser.readline())
-    print("RECEIVED HANDSHAKE")
-    ser.write('go'.encode('utf-8'))
-    while semantic != "RECEIVED HANDSHAKE":
-        semantic = myModel.parseSerial(ser.readline())
-    print("SUCCESSFUL PAIRING")
-
-
-def calibrate():
-    global BIAS
-    answer = input("load from existing data? (y, n)")
-    if answer == "y":
-        try:
-            BIAS = pickle.load(open("BIAS.pkl", "rb"))
-        except:
-            print("whoops! file doesn't exist.")
-            quit()
-    else:
-        truth = int(input("what is the baseline?"))
-        measured, _ = getSample(10, 0.3)
-        print("the offset is " + str(truth - measured))
-        answer = input("do you want to assign this bias to the entire system? (y, n)")
-        if answer == "y":
-            BIAS = truth - measured
-            answer = input("save? (y, n)")
-            if answer == "y":
-                pickle.dump(BIAS, open("BIAS.pkl", "wb"))
-        else:
-            print("calibration data discarded")
+    ser = serialHelp.connect(portvalue="COM5")
+    serialHelp.pair(ser)
 
 def calculateVandT():
     global succ
@@ -125,71 +87,17 @@ def checkLights(temperature, laststatus):
 
     return laststatus
 
-def getSample(number, threshold):
-
-    counter = 0
-    total = 0
-    totalv = 0
-    giveupcount = 0
-    ser.reset_input_buffer()
-    while(counter < number):
-        voltage, temperature = calculateVandT()
-        print("getting value " + str(counter) + ", which is " + str(round(temperature, 3)))
-        if(counter == 0):
-            runningavg = temperature
-        else:
-            runningavg = total/(counter)
-        if(temperature > (runningavg + threshold) or temperature < (runningavg - threshold)):
-            print("dropped value")
-            giveupcount += 1
-        else:
-            total += temperature
-            totalv += voltage
-            counter += 1
-        if(giveupcount > 10):
-            break #this prevents bogus values from holding up the code
-
-
-    return (total/number + BIAS), totalv/number
-
-
-
 def main():
     laststatus = "OFF"
-    semantic = "y"
-    counter = 0
+
     setup()
-    '''
-    status = input("are you in calibrate mode? (y, n)")
-    if (status == "y"):
-        calibrate()
-    '''
 
     while True:
-        '''
-        if counter % 20 == 0:
-            semantic = input("What do you want? Continuous (c), sample (s)")
-        '''
-        semantic = "c"
-        if semantic == "c":
-            ser.reset_input_buffer()
-            voltage, temperature = calculateVandT()
-            temperature = temperature + BIAS
-            print("The voltage is: " + str(round(voltage,5)) + "V. The calculated temperature is: " + str(round(temperature,2)) + " degrees Celsius")
-            laststatus = checkLights(temperature, laststatus)
-
-
-            counter += 1
-        elif semantic == "s":
-            num = int(input("how many values do you want to average?"))
-            threshold = float(input("at what threshold?"))
-            avgtemp, avgvolt = getSample(num, threshold)
-            avgtempadj = avgtemp + BIAS
-            print("The average voltage is: " + str(round(avgvolt, 2)) + "V. The average calculated temperature (adjusted) is: " + str(
-                round(avgtempadj, 2)) + " degrees Celsius")
-            input("press enter to continue")
-            counter = 20 #kinda a stupid way to get a new option to come about
-
+        ser.reset_input_buffer()
+        voltage, temperature = calculateVandT()
+        temperature = temperature + BIAS
+        print("The voltage is: " + str(round(voltage,5)) + "V. The calculated temperature is: " + str(round(temperature,2)) + " degrees Celsius")
+        laststatus = checkLights(temperature, laststatus)
 
 
 if __name__ == '__main__':
